f2f7570d57102c329485d42b0e68ad99bcc53ec6
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-10-16 03:30:40 -0700

    Put radial circle in background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import logo from './assets/momentlogo.svg';
+import appscreen from './assets/momentmainpage.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Logo" />
-        <p>
+        <img src={logo} className="Moment-logo" alt="Logo" />
+        <p className="Subtitle-text">
           An app for all kinds of communities.<br/>
           Made by students. For students.
-        </p>
-        <p>
-          
+          <img src={appscreen} className="Phone-screen" alt="Screen" />
         </p>
       </header>
     </div>
