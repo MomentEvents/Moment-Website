32e956737266d008eb54d4680c4363c5f0b8408f
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-10-16 06:05:44 -0700

    Centered title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,19 +17,21 @@ const Header = () => {
   return (
     <section id="header">
       <div className="container header">
-        <div className="header-left" data-aos="fade-right">
-          <h1>
-            <span>Finding events is hard</span>
-            <span>Let's make it easier</span>
-            {/* <span>is a Lorem ipsum dolor sit amet, consectetur adipisicing</span> */}
-          </h1>
-          <p className="u-text-small">
-            Moment is a university events mobile app
-          </p>
-          {/* <div className="header-cta">
+        <div className="aligning-contents-center">
+          <div className="header-left" data-aos="fade-right">
+            <h1>
+              <span>Finding events is hard</span>
+              <span>Let's make it easier</span>
+              {/* <span>is a Lorem ipsum dolor sit amet, consectetur adipisicing</span> */}
+            </h1>
+            <p className="u-text-large">
+              A university events app is coming very soon to UCSD and UIUC!
+            </p>
+            {/* <div className="header-cta">
             <Button text={"Get Started"} btnClass={"btn-dark"} href={"#"} />
             <Button text={"How It Works"} btnClass={"btn-light"} href={"#"} />
           </div> */}
+          </div>
         </div>
         {/* <div className="header-right" data-aos="fade-left">
           <img src={phoneHeader} alt="phone" />
