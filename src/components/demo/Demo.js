2f2f593e58483096ffb4fd54b13fdf56a6526078
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-02-20 16:44:29 -0800

    Added bottom 3 screenshots

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import "./Demo.css";
 
-import personalCalendar from "../../assets/PersonalCalendar.png";
-import searchEvent from "../../assets/SearchEvent.png";
-import selectTags from "../../assets/SelectTags.png";
+import joinedEvents from "../../assets/mock-joined-events.png";
+import searchEvents from "../../assets/mock-search-events.png";
+import hostedEvents from "../../assets/mock-hosted-events.png";
 
 
 import AOS from "aos";
@@ -23,17 +23,17 @@ const Demo = () => {
           <div className="demo-content">
             <div className="demo-left" data-aos="fade-right">
                 <h1>Calendar</h1>
-              <img src={personalCalendar} alt="Calendar" />
+              <img src={joinedEvents} alt="Calendar" />
               <h2>Have an organized calendar view of all the events you plan on attending.</h2>
             </div>
             <div className="demo-middle" data-aos="fade-up">
                 <h1>Search</h1>
-                <img src={searchEvent} alt="Search" />
+                <img src={searchEvents} alt="Search" />
                 <h2>Search for any upcoming event that is bound to happen.</h2>
             </div>
             <div className="demo-right" data-aos="fade-left">
                 <h1>Host</h1>
-                <img src={selectTags} alt="Tags" />
+                <img src={hostedEvents} alt="Tags" />
                 <h2>See all the events you are hosting, with a list of people who are joining your events.</h2>
             </div>
           </div>
