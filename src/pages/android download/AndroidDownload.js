8849687fbcf5d71de9d63b0c976a69cdc7226a9a
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-03-13 20:02:15 -0700

    Fixed rerouting. Added image link preview

--- src/pages/android download/AndroidDownload.js
+++ src/pages/android download/AndroidDownload.js
@@ -3,7 +3,16 @@ import { useEffect } from "react";
 
 export default function AndroidDownload() {
   useEffect(() => {
-    window.location.assign("https://play.google.com/store/apps/details?id=com.moment.eventsapp");
+    window.location.assign(
+      "https://play.google.com/store/apps/details?id=com.moment.eventsapp"
+    );
   }, []);
-  return <div>Redirecting...</div>;
+  return (
+    <div className="download">
+      Redirecting. Still waiting? Click{" "}
+      <a href="https://play.google.com/store/apps/details?id=com.moment.eventsapp">
+        here.
+      </a>
+    </div>
+  );
 }
--- src/pages/ios download/IOSDownload.js
+++ src/pages/ios download/IOSDownload.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 
 export default function IOSDownload() {
-    useEffect(() => {
-        window.location.assign('https://apps.apple.com/us/app/moment-events/id1671447709');
-    }, [])
+  useEffect(() => {
+    window.location.assign(
+      "https://apps.apple.com/us/app/moment-events/id1671447709"
+    );
+  }, []);
   return (
-    <div>Redirecting...</div>
-  )
+    <div className="download">
+      Redirecting. Still waiting? Click{" "}
+      <a href="https://apps.apple.com/us/app/moment-events/id1671447709">
+        here.
+      </a>
+    </div>
+  );
 }
